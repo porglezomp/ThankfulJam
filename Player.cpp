e9257f8f9d17ada4d2e39ff5c01b98933ab4cdca
Blade Nelson <powderblock@gmail.com>
2013-11-29 14:09:16 -0500

    Added ints for width and height. Added screen limitations for the player.

--- Player.cpp
+++ Player.cpp
@@ -110,4 +110,19 @@ void Player::update() {
 	} else if (motion.yrecent == PM_DOWN) {
 		y -= speed * fac;
 	}
+	if(x < -12.83){
+		x += speed;
+	}
+	
+	if(y > 9.5){
+		y -= speed;
+	}
+	
+	if(x > 12.83){
+		x -= speed;
+	}
+	
+	if(y < -9.5){
+		y += speed;
+	}
 }
