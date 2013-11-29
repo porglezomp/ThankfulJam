2498c443135be8a219ede534731b93689d557191
Caleb Jones <bionicle.dragon@gmail.com>
2013-11-29 10:08:20 -0500

    Moved the mainloop into a function Added a gamemode variable (useful for menus later) Added more comments to main.cpp The event loop now passes keydown events on to the player The mainloop now calls player.update()

--- main.cpp
+++ main.cpp
@@ -7,46 +7,59 @@ SDL_Surface *screen;
 bool running = true;
 //Used for FPS capping:
 int FPS = 60;
-
 Uint32 start;
+//The player
 Player player;
+//What to do right now
+int gamemode = 1;
 
 //Functions:
 void quit(int);
 void render();
 void events();
 void matchfps();
+int mainloop();
 
+//main
 int main(int argc, char *argv[]) {
+	//Setup SDL and OpenGL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) quit(1);
 	screen = SDL_SetVideoMode(640, 480, 32, SDL_OPENGL);
+	if (screen == NULL) quit(2);
 	SDL_WM_SetCaption("Thankful Jam", NULL);
 	glColor3f(0, 0, 0);
+
 	while (running) {
 		start = SDL_GetTicks();
-		render();
-		events();
+		if (gamemode == 1) gamemode = mainloop();
 		matchfps();
 	}
+
 	quit(0);
 }
 
 void render() {
+	//Clear
 	glClearColor(1, 1, 1, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//Draw the player
 	player.draw();
+	//Update the screen
 	SDL_GL_SwapBuffers();
 }
 
+//Handle events
 void events() {
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
 		switch (event.type) {
+			//Handle quit events
 			case SDL_QUIT:
 				running = false;
 				break;
 			case SDL_KEYDOWN:
 				player.input(event);
+				//Also quit on the escape key
 				if (event.key.keysym.sym == SDLK_ESCAPE) {
 					running = false;
 				}
@@ -55,12 +68,25 @@ void events() {
 	}
 }
 
+//Quits and handles cleanup
+//Because apparently atexit() is evil
 void quit(int rc) {
+	SDL_Quit();
 	exit(rc);
 }
 
+//Waits for the remainder of the frame (until 1/FPS of a second)
 void matchfps() {
 	if(1000/FPS > SDL_GetTicks()-start){
 		SDL_Delay(1000/FPS-(SDL_GetTicks()-start));
 	}
+}
+
+//The only loop right now
+int mainloop() {
+	player.update();
+	render();
+	events();
+	//Stay in the mainloop
+	return 1;
 }
