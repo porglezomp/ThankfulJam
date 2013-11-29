2ff4e7d5a0b2d15f8deefa1779a1d98c91ab2b23
Caleb Jones <bionicle.dragon@gmail.com>
2013-11-28 21:43:47 -0500

    Added a mainloop with event handling. Made the background clear properly. Moved some code into functions.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,58 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_opengl.h>
 
+//variables
 SDL_Surface *screen;
+bool running = true;
+int FPS = 60;
+Uint32 start;
 
+//functions
 void quit(int);
+void render();
+void events();
+void matchfps();
 
 int main(int argc, char *argv[]) {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) quit(1);
 	screen = SDL_SetVideoMode(640, 480, 32, SDL_OPENGL);
-	SDL_GL_SwapBuffers();
-	SDL_Delay(1000);
+	SDL_WM_SetCaption("Thankful Jam", NULL);
+	while (running) {
+		start = SDL_GetTicks();
+		render();
+		events();
+		matchfps();
+	}
 	quit(0);
+}
+
+void render() {
+	glClearColor(1, 1, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	SDL_GL_SwapBuffers();
+}
+
+void events() {
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+			case SDL_KEYDOWN:
+				if (event.key.keysym.sym == SDLK_ESCAPE) {
+					running = false;
+				}
+				break;
+		}
+	}
 }
 
 void quit(int rc) {
 	exit(rc);
+}
+
+void matchfps() {
+	if(1000/FPS > SDL_GetTicks()-start)
+		SDL_Delay(1000/FPS-(SDL_GetTicks()-start));
 }
